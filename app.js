4870998d39796f0e2d6cb8e3a80e3b11d4d840f7
Jansen Kwong <j.kwong4@student.unimelb.edu.au>
2019-03-27 15:23:39 +1100

    updated app.js again

--- app.js
+++ app.js
@@ -12,190 +12,44 @@ var characters = [
         "height": "172",
         "mass": "77",
         "hair_color": "blond",
-        "skin_color": "fair",
-        "eye_color": "blue",
-        "birth_year": "19BBY",
-        "gender": "male",
-        "homeworld": "https://swapi.co/api/planets/1/",
-        "films": [
-            "https://swapi.co/api/films/2/",
-            "https://swapi.co/api/films/6/",
-            "https://swapi.co/api/films/3/",
-            "https://swapi.co/api/films/1/",
-            "https://swapi.co/api/films/7/"
-        ],
-        "species": [
-            "https://swapi.co/api/species/1/"
-        ],
-        "vehicles": [
-            "https://swapi.co/api/vehicles/14/",
-            "https://swapi.co/api/vehicles/30/"
-        ],
-        "starships": [
-            "https://swapi.co/api/starships/12/",
-            "https://swapi.co/api/starships/22/"
-        ],
-        "created": "2014-12-09T13:50:51.644000Z",
-        "edited": "2014-12-20T21:17:56.891000Z",
-        "url": "https://swapi.co/api/people/1/"
+        
     },
     {
         "name": "C-3PO",
         "height": "167",
         "mass": "75",
         "hair_color": "n/a",
-        "skin_color": "gold",
-        "eye_color": "yellow",
-        "birth_year": "112BBY",
-        "gender": "n/a",
-        "homeworld": "https://swapi.co/api/planets/1/",
-        "films": [
-            "https://swapi.co/api/films/2/",
-            "https://swapi.co/api/films/5/",
-            "https://swapi.co/api/films/4/",
-            "https://swapi.co/api/films/6/",
-            "https://swapi.co/api/films/3/",
-            "https://swapi.co/api/films/1/"
-        ],
-        "species": [
-            "https://swapi.co/api/species/2/"
-        ],
-        "vehicles": [],
-        "starships": [],
-        "created": "2014-12-10T15:10:51.357000Z",
-        "edited": "2014-12-20T21:17:50.309000Z",
-        "url": "https://swapi.co/api/people/2/"
+       
     },
     {
         "name": "R2-D2",
         "height": "96",
         "mass": "32",
         "hair_color": "n/a",
-        "skin_color": "white, blue",
-        "eye_color": "red",
-        "birth_year": "33BBY",
-        "gender": "n/a",
-        "homeworld": "https://swapi.co/api/planets/8/",
-        "films": [
-            "https://swapi.co/api/films/2/",
-            "https://swapi.co/api/films/5/",
-            "https://swapi.co/api/films/4/",
-            "https://swapi.co/api/films/6/",
-            "https://swapi.co/api/films/3/",
-            "https://swapi.co/api/films/1/",
-            "https://swapi.co/api/films/7/"
-        ],
-        "species": [
-            "https://swapi.co/api/species/2/"
-        ],
-        "vehicles": [],
-        "starships": [],
-        "created": "2014-12-10T15:11:50.376000Z",
-        "edited": "2014-12-20T21:17:50.311000Z",
-        "url": "https://swapi.co/api/people/3/"
     },
     {
         "name": "Darth Vader",
         "height": "202",
         "mass": "136",
         "hair_color": "none",
-        "skin_color": "white",
-        "eye_color": "yellow",
-        "birth_year": "41.9BBY",
-        "gender": "male",
-        "homeworld": "https://swapi.co/api/planets/1/",
-        "films": [
-            "https://swapi.co/api/films/2/",
-            "https://swapi.co/api/films/6/",
-            "https://swapi.co/api/films/3/",
-            "https://swapi.co/api/films/1/"
-        ],
-        "species": [
-            "https://swapi.co/api/species/1/"
-        ],
-        "vehicles": [],
-        "starships": [
-            "https://swapi.co/api/starships/13/"
-        ],
-        "created": "2014-12-10T15:18:20.704000Z",
-        "edited": "2014-12-20T21:17:50.313000Z",
-        "url": "https://swapi.co/api/people/4/"
     },
     {
         "name": "Leia Organa",
         "height": "150",
         "mass": "49",
         "hair_color": "brown",
-        "skin_color": "light",
-        "eye_color": "brown",
-        "birth_year": "19BBY",
-        "gender": "female",
-        "homeworld": "https://swapi.co/api/planets/2/",
-        "films": [
-            "https://swapi.co/api/films/2/",
-            "https://swapi.co/api/films/6/",
-            "https://swapi.co/api/films/3/",
-            "https://swapi.co/api/films/1/",
-            "https://swapi.co/api/films/7/"
-        ],
-        "species": [
-            "https://swapi.co/api/species/1/"
-        ],
-        "vehicles": [
-            "https://swapi.co/api/vehicles/30/"
-        ],
-        "starships": [],
-        "created": "2014-12-10T15:20:09.791000Z",
-        "edited": "2014-12-20T21:17:50.315000Z",
-        "url": "https://swapi.co/api/people/5/"
     },
     {
         "name": "Owen Lars",
         "height": "178",
         "mass": "120",
         "hair_color": "brown, grey",
-        "skin_color": "light",
-        "eye_color": "blue",
-        "birth_year": "52BBY",
-        "gender": "male",
-        "homeworld": "https://swapi.co/api/planets/1/",
-        "films": [
-            "https://swapi.co/api/films/5/",
-            "https://swapi.co/api/films/6/",
-            "https://swapi.co/api/films/1/"
-        ],
-        "species": [
-            "https://swapi.co/api/species/1/"
-        ],
-        "vehicles": [],
-        "starships": [],
-        "created": "2014-12-10T15:52:14.024000Z",
-        "edited": "2014-12-20T21:17:50.317000Z",
-        "url": "https://swapi.co/api/people/6/"
     },
     {
         "name": "Beru Whitesun lars",
         "height": "165",
         "mass": "75",
         "hair_color": "brown",
-        "skin_color": "light",
-        "eye_color": "blue",
-        "birth_year": "47BBY",
-        "gender": "female",
-        "homeworld": "https://swapi.co/api/planets/1/",
-        "films": [
-            "https://swapi.co/api/films/5/",
-            "https://swapi.co/api/films/6/",
-            "https://swapi.co/api/films/1/"
-        ],
-        "species": [
-            "https://swapi.co/api/species/1/"
-        ],
-        "vehicles": [],
-        "starships": [],
-        "created": "2014-12-10T15:53:41.121000Z",
-        "edited": "2014-12-20T21:17:50.319000Z",
-        "url": "https://swapi.co/api/people/7/"
     }]
 
     app.get('/',function(req, res){
@@ -205,63 +59,32 @@ var characters = [
     
     app.get('/:id',function(req,res){
         res.send(characters[req.params.id]);
-    })
+    });
 
-    // app.post('/add',function(req,res){
-    //     var newChar = {
-    //         name: req.body.name,
-    //         height: req.body.height,
-    //         mass: req.body.mass,
-    //         hair_color: req.body.hair_color,
-    //         skin_color: req.body.skin_color,
-    //         eye_color: req.body.eye_color,
-    //         birth_year: req.body.birth_year,
-    //         gender: req.body.gender,
-    //         homeworld: req.body.homeworld,
-    //         films: req.body.films,
-    //         species: req.body.species,
-    //         vehicles: req.body.vehicles,
-    //         starships: req.body.starships,
-    //         created: req.body.created,
-    //         edited: req.body.edited,
-    //         url: req.body.url
-    //     }
-    //     res.send(newChar);
-    // })
-    // app.post('/a]ddit',function(req,res){
-    //     var newChar = {
-	// 		"name": "R5-D4",
-	// 		"height": "97",
-	// 		"mass": "32",
-	// 		"hair_color": "n/a",
-	// 		"skin_color": "white, red",
-	// 		"eye_color": "red",
-	// 		"birth_year": "unknown",
-	// 		"gender": "n/a",
-	// 		"homeworld": "https://swapi.co/api/planets/1/",
-	// 		"films": [
-	// 			"https://swapi.co/api/films/1/"
-	// 		],
-	// 		"species": [
-	// 			"https://swapi.co/api/species/2/"
-	// 		],
-	// 		"vehicles": [],
-	// 		"starships": [],
-	// 		"created": "2014-12-10T15:57:50.959000Z",
-	// 		"edited": "2014-12-20T21:17:50.321000Z",
-	// 		"url": "https://swapi.co/api/people/8/"
-    //     };
-    //     characters.push(newChar);
-    //     res.send(newChar);
-    //     console.log("Character added.");
-    // });
+    app.post('/add',function(req,res){
+        var newChar = {
+            name: req.body.name,
+            height: req.body.height,
+            mass: req.body.mass,
+            hair_color: req.body.hair_color,
+        }
+        characters.push(newChar);
+        res.send(newChar);
+    });
 
-    app.delete('/delete/:id', function(req,res){
-        if(req.params.id < characters.length -1){
-            characters.splice(req.params.id,1);
+
+    app.delete('/del/:name', function(req,res){
+        
+        for(let i =0; i< characters.length;i++){
+            if(characters[i].name === req.params.name){
+                characters.splice(i,1);
+                break;
+            }
         }
         res.send(characters);
-    })
+
+    });
+
 
     app.listen(port, function(){
         console.log("Done");
